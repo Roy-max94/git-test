dae84835ad8b2ff84be86bad094d8e926f985d55
Rox-max94 <loyi.ho@foxmail.com>
2021-10-04 20:44:15 +0800

    first commit

---
+++ git-test/src/main/java/com/roy/demo/HelloGit.java
@@ -0,0 +1,14 @@
+package com.roy.demo;
+
+/**
+ * @Package: com.roy.demo
+ * @ClassName: HelloGit
+ * @Author: 明天
+ * @CreateTime: 2021/10/4 20:24
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
